d632db0d2560f3b2f3ef575c12cd5f143029758f
Dominik Schöni <dominik@dominikschoeni.ch>
2016-09-17 01:04:40 +0200

    pdf-upload

--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 @Component({
     template: require('./upload.component.html'),
     styles: [require('./upload.component.scss')],
@@ -8,8 +7,23 @@ import { Component } from '@angular/core';
 
 export class UploadComponent {
 
+    private options: any = {
+      url: 'http://localhost:5000/upload',
+      filterExtensions: true,
+      allowedExtensions: ['application/pdf'],
+      calculateSpeed: true
+    }
+
+    private progress: number = 0;
+    private response: any = {};
+
     constructor() {
 
+    }
+
+    handleUpload(data: any): void {
+      this.response = data;
+      this.progress = Math.floor(data.progress.percent / 100);
     }
 
 }
